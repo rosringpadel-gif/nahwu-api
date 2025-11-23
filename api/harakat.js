fd44b5f575692b0cec94d214d66165ee9e7be211
rosringpadel-gif <rosringpadel@gmail.com>
2025-11-24 03:40:16 +0700

    harakat.js

---
+++ api/harakat.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/CAMeL-Lab/arabic-text-diacritizer",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${process.env.HF_API_KEY}`
+        },
+        body: JSON.stringify({ inputs: req.body.text })
+      }
+    );
+
+    const data = await response.json();
+    res.status(200).json({ result: data[0].generated_text });
+  } catch (err) {
+    res.status(500).json({ error: err.toString() });
+  }
+}
